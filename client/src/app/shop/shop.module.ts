81c679dbe2b1ff47630fa696584f74847d15f072
Pete Tran <your.email@example.com>
2023-08-06 14:28:25 -0400

    implemented basket and total

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -5,6 +5,7 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ShopRoutingModule } from './shop-routing.module';
+import { BasketRoutingModule } from '../basket/basket-routing.module';
 
 
 
@@ -17,7 +18,8 @@ import { ShopRoutingModule } from './shop-routing.module';
   imports: [
     CommonModule,
     SharedModule,
-    ShopRoutingModule
+    ShopRoutingModule,
+    BasketRoutingModule
   ],
 })
 export class ShopModule { }
